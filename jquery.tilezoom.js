fca7e18dfd2e8f92cfc958c6ba8fc4843174d48e
ematsakov <ematsakov@gmail.com>
2015-12-02 17:29:43 +0200

    Changed version to 1.2

--- jquery.tilezoom.js
+++ jquery.tilezoom.js
@@ -2,7 +2,7 @@
  * jQuery WCS Tile Zoom Plugin
  * Examples and documentation at: http://demo.webcodingstudio.com/tile-zoom/
  * Copyright (c) 2011 Evgeny Matsakov
- * Version: 1.0 (2-NOV-2011)
+ * Version: 1.2 (2-DEC-2015)
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
